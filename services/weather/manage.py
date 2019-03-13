23467fe7ba3b2da4acc679caf45bd81c7a288ea2
Carlos Mario Herrera <cmarioherrera@gmail.com>
2019-03-13 10:54:27 -0500

    cli commnad created db, seed_db

--- services/weather/manage.py
+++ services/weather/manage.py
@@ -2,11 +2,25 @@
 
 from flask.cli import FlaskGroup
 
-from src import create_app
+from src import create_app, db
+from src.api.models import Station
 
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
 
+
+@cli.command()
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+@cli.command('seed_db')
+def seed_db():
+    """Seeds the database"""
+    db.session.add(Station(temperature=25.8, humidity=58.3))
+    db.session.add(Station(temperature=28.6, humidity=45.6))
+    db.session.commit()
 
 
 if __name__ == '__main__':
