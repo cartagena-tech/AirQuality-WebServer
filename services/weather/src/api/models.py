d07da271c64c705247471a3c42177b37c66b2314
Carlos Mario Herrera <cmarioherrera@gmail.com>
2019-03-13 10:46:40 -0500

    Station model added

--- services/weather/src/api/models.py
+++ services/weather/src/api/models.py
@@ -1,2 +1,30 @@
 # services/weather/src/api/models.py
+""" Station model."""
 
+# sqlalchemy
+from sqlalchemy.sql import func
+# instance db
+from src import db
+
+
+class Station(db.Model):
+    """Station model.
+    """
+    __tablename__ = 'stations'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    temperature = db.Column(db.Float(), nullable=False)
+    humidity = db.Column(db.Float(), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+    created_date = db.Column(db.DateTime, default=func.now(), nullable=False)
+
+    def __init__(self, temperature, humidity):
+        self.temperature = temperature
+        self.humidity = humidity
+
+    def to_json(self):
+        return {
+            'temperature': self.temperature,
+            'humidity': self.humidity,
+            'active': self.active,
+            'date': self.created_date
+        }
